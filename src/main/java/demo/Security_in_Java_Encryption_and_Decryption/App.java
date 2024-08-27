3cee8fa4fbed31facbe273439aed19fabaa65e1c
DatMV <datmv01@gmail.com>
2024-08-27 09:11:44 +0700

    first commit

---
+++ src/main/java/demo/Security_in_Java_Encryption_and_Decryption/App.java
@@ -0,0 +1,13 @@
+package demo.Security_in_Java_Encryption_and_Decryption;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
